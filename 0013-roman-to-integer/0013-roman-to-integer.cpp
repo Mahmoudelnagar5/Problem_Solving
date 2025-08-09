e4d1af61fc5f4f1a999a639b0e86ba38a878d045
Mahmoud Elnagar <126767234+Mahmoudelnagar5@users.noreply.github.com>
2025-08-09 15:18:45 +0300

    Time: 7 ms (51.68%), Space: 13.7 MB (5.49%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        
+         map<char,int> ans = {{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
+            int sum = 0;
+            for(int i = 0;i  < s.size();i++){
+                if(ans[s[i]] < ans[s[i + 1]])
+                  sum -= ans[s[i]];
+                else
+                  sum += ans[s[i]];
+            }
+            return sum;
     }
 };
